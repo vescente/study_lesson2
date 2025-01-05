2c2355881d4602a7c786d6afa6979f7c693e8bac
Nikolas <the.shabalin@gmail.com>
2025-01-05 14:03:46 +0300

    Add employee variables and type output in variables.py

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Объект: сотрудник
+
+# Объявление переменных
+name = "Иван Иванов"  # строка
+age = 35  # целое число
+salary = 75000.50  # число с плавающей точкой
+is_manager = True  # логический тип
+department = "Sales"  # строка
+years_of_experience = 10  # целое число
+performance_rating = 4.7  # число с плавающей точкой
+
+# Вывод типов переменных
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_manager))
+print(type(department))
+print(type(years_of_experience))
+print(type(performance_rating))
